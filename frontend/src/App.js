4a3f1d7a8b84549830c6e695f61cf8ff770db255
Paul Devlin <pmdevlin360@gmail.com>
2022-09-02 00:23:23 -0500

    home page set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 //import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import style from "./styles/App.module.css";
 
 const App = () => {
-  const [comments, setComments] = useState("");
-
   return (
     <div className={style.app}>
       {/* <Navbar /> */}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import style from "../styles/Home.module.css";
+import logo from "../images/Logo2.svg";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={style.container}>
+      <img className={style.logo} src={logo} alt="logo" />
+
+      <div className={style.intro}>
+        <h1>Hello, I am </h1>
+        <h1 className={style.name}>Paul Devlin</h1>
+      </div>
+      <h1 className={style.text}>I'm a full-stack web developer.</h1>
+
+      <button className={style.button} type="button">
+        Projects
+      </button>
+    </div>
+  );
 }
