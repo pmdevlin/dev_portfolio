a738bed33785ac5766b8ef7cf29bd87e3030049e
Paul Devlin <pmdevlin360@gmail.com>
2022-09-07 18:45:13 -0500

    adding design to about page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,9 +3,20 @@ import style from "../styles/About.module.css";
 import Navbar from "../components/Navbar";
 const About = () => {
   return (
-    <div className={style.container}>
+    <>
       <Navbar />
-    </div>
+      <div className={style.container}>
+        <div className={style.L}>
+          <h2 className={style.introTitle}>About Me</h2>
+          <div className={style.intro}></div>
+        </div>
+        <div className={style.R}>
+          <div className={style.image}></div>
+          <h2 className={style.skillsTitle}>Some of my Skills</h2>
+          <div className={style.skills}></div>
+        </div>
+      </div>
+    </>
   );
 };
 
