007ad964e0ad9f57a51a8450d02fcf2eeeb4d15d
Paul Devlin <pmdevlin360@gmail.com>
2022-09-10 07:20:54 -0500

    adding comment display component

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -2,7 +2,21 @@ import React from "react";
 import style from "./Display.module.css";
 
 const Display = () => {
-  return <div className={style.container}></div>;
+  return (
+    <div className={style.container}>
+      <div className={style.nameContainer}>
+        <label className={style.name}>Name:</label>
+        <h3 className={style.input}> temp name</h3>
+      </div>
+      <div className={style.topicContainer}>
+        <label className={style.topic}>Topic:</label>
+        <h3 className={style.input}>code theory</h3>
+      </div>
+      <div className={style.paragraphContainer}>
+        <p className={style.input}>this is a temp comment</p>
+      </div>
+    </div>
+  );
 };
 
 export default Display;
