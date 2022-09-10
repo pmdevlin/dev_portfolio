c3bebc2c448bc6754a01857ab5e491529fb03012
Paul Devlin <pmdevlin360@gmail.com>
2022-09-10 06:22:19 -0500

    commment box component

--- frontend/src/components/CommentBox.js
+++ frontend/src/components/CommentBox.js
@@ -6,23 +6,22 @@ const CommentBox = () => {
     <div className={style.container}>
       <div className={style.main}>
         <form className={style.form}>
-          <label className={style.Title} for="name">
+          <label className={style.title} for="name">
             Name
           </label>
-          <input className={style.nameBox} type="text" id="name" name="name" />
-          <label className={style.Title} for="topic">
+          <input className={style.nameBox} type="text" name="name" />
+          <label className={style.title} for="topic">
             Topic
           </label>
-          <input
-            className={style.topicBox}
-            type="text"
-            id="topic"
-            name="topic"
-          />
-          <label className={style.Title} for="body">
+          <input className={style.topicBox} type="text" name="topic" />
+          <label className={style.title} for="body">
             Comment
           </label>
-          <input className={style.bodyBox} type="text" id="body" name="body" />
+          <textarea
+            className={style.bodyBox}
+            type="text"
+            name="body"
+          ></textarea>
           <input className={style.button} type="submit" value="submit" />
         </form>
       </div>
---
+++ frontend/src/components/Display.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Display.module.css";
+
+const Display = () => {
+  return <div className={style.container}></div>;
+};
+
+export default Display;
