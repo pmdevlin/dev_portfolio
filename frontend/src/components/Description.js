5ee6ff065fe7c245f443c9821a56cbf82bfa316a
pmdevlin <pmdevlin360@gmail.com>
2022-12-05 16:45:58 -0600

    made cosmetic changes still working towards fixing the state on the comments page

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -1,19 +1,35 @@
 import React from "react";
 import style from "../componentStyles/Description.module.css";
 import { FaWindowClose } from "react-icons/fa";
+// import photo from "../utils/Photos"
 
 const Description = (props) => {
+  const { name, tech, info, link } = props.projectObj;
+
   const descriptionClose = () => {
     props.setVisible(false);
   };
 
   return (
-    <div className={style.container}>
-      <div className={style.titleBox}>
-        <h2>{props.projectObj.name}</h2>
-        <FaWindowClose onClick={descriptionClose} />
+    <div className={style.master}>
+      <div className={style.photos}>
+        <img
+          className={style.thumbnails}
+          src="frontend\src\utils\Photos\codeTheoryHome.png"
+          alt=""
+        ></img>
       </div>
-      <p className={style.info}>{props.projectObj.info}</p>
+      <div className={style.container}>
+        <div className={style.titleBox}>
+          <h2>{name}</h2>
+          <FaWindowClose onClick={descriptionClose} />
+        </div>
+        <p className={style.tech}>{tech}</p>
+        <p className={style.info}>{info}</p>
+        <a href={link} className={style.link} rel="noreferrer" target="_blank">
+          Code Theory Repo
+        </a>
+      </div>
     </div>
   );
 };
--- frontend/src/utils/projectObj.js
+++ frontend/src/utils/projectObj.js
@@ -1,8 +1,37 @@
+import style from "../componentStyles/Description.module.css";
+
 const projectObj = [
   {
     id: 0,
     name: "Code Theory",
-    info: "this is a temp body description",
+    tech: " (JavaScript, React, Node, Express, Axios, PostgreSQL, GitLab, figma)",
+    info: (
+      <div className={style.lists}>
+        <h4>
+          Developed a Full-Stack app to help visually representatives sort
+          algorithms.{" "}
+        </h4>
+        <ul>
+          <li>
+            Collaborated closely with a team of six engineers to design and
+            implement innovative solutions for algorithmic learning with a
+            front-end development web application.
+          </li>
+          <li>
+            Participated in Front-end Design and state management using ReactJS,
+            while troubleshooting foundational application bugs and handling
+            ticket management in a GitLab workflow.
+          </li>
+          <li>
+            Designed and diagnosed NodeJS and Express HTTP server and queries
+            for data delivery between a PostgreSQL database and ReactJS
+            front-end for user comments.
+          </li>
+        </ul>
+      </div>
+    ),
+
+    link: "https://gitlab.com/m7093/frontend-capstone/-/tree/main",
   },
   {
     id: 1,
