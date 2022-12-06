4203ba681bb4345c50470cd9be7fdb36e134b977
pmdevlin <pmdevlin360@gmail.com>
2022-12-06 17:47:35 -0600

    a lot of changes to the projects [added model for project descriptions, currently adding graphics to discription]

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -1,34 +1,38 @@
 import React from "react";
 import style from "../componentStyles/Description.module.css";
 import { FaWindowClose } from "react-icons/fa";
-// import photo from "../utils/Photos"
 
 const Description = (props) => {
-  const { name, tech, info, link } = props.projectObj;
+  const { name, tech, info, link, graphics } = props.projectObj;
 
+  //console.log(graphics);
   const descriptionClose = () => {
     props.setVisible(false);
   };
 
+  const graphicDisplay = () => {
+    graphics.map((item) => {
+      return <img src={item}></img>;
+    });
+  };
+
   return (
     <div className={style.master}>
-      <div className={style.photos}>
-        <img
-          className={style.thumbnails}
-          src="frontend\src\utils\Photos\codeTheoryHome.png"
-          alt=""
-        ></img>
-      </div>
       <div className={style.container}>
         <div className={style.titleBox}>
           <h2>{name}</h2>
           <FaWindowClose onClick={descriptionClose} />
         </div>
-        <p className={style.tech}>{tech}</p>
-        <p className={style.info}>{info}</p>
+        <span className={style.tech}>{tech}</span>
+        <span className={style.info}>{info}</span>
         <a href={link} className={style.link} rel="noreferrer" target="_blank">
           Code Theory Repo
         </a>
+        <div className={style.photos}>
+          <img className={style.thumbnails} src={graphics[0]} alt=""></img>
+          <img className={style.thumbnails} src={graphics[1]} alt=""></img>
+          <img className={style.thumbnails} src={graphics[2]} alt=""></img>
+        </div>
       </div>
     </div>
   );
--- frontend/src/utils/projectObj.js
+++ frontend/src/utils/projectObj.js
@@ -1,16 +1,24 @@
 import style from "../componentStyles/Description.module.css";
+import CThomeImage from "./Photos/codeTheoryHome.png";
+import CTmenuImage from "./Photos/codeTheoryMenu.png";
+import CTsortImage from "./Photos/codeTheorySort.png";
+// import srbHome from "./Photos/srbHome";
+// import srbAdd from "./Photos/srbAdd";
+// import srbCards from "./Photos/srbCards";
 
 const projectObj = [
   {
     id: 0,
     name: "Code Theory",
     tech: " (JavaScript, React, Node, Express, Axios, PostgreSQL, GitLab, figma)",
+    link: "https://gitlab.com/m7093/frontend-capstone/-/tree/main",
+    graphics: [CThomeImage, CTmenuImage, CTsortImage],
     info: (
       <div className={style.lists}>
-        <h4>
-          Developed a Full-Stack app to help visually representatives sort
-          algorithms.{" "}
-        </h4>
+        <h3>
+          Developed a Full-Stack app to help visually represent sorting
+          algorithms.
+        </h3>
         <ul>
           <li>
             Collaborated closely with a team of six engineers to design and
@@ -30,13 +38,38 @@ const projectObj = [
         </ul>
       </div>
     ),
-
-    link: "https://gitlab.com/m7093/frontend-capstone/-/tree/main",
   },
   {
     id: 1,
     name: "SRB (software resource buddy)",
-    info: "this is a temp body description",
+    tech: " (JavaScript, React, Node, Express, Axios, PostgreSQL, GitLab, figma)",
+    link: "https://github.com/pmdevlin/Software-Resource-Buddy-SRB-",
+    // graphics: [srbHome, srbCards, srbAdd],
+    info: (
+      <div className={style.lists}>
+        <h3>
+          Developed a Full-Stack app to help visually represent sorting
+          algorithms.
+        </h3>
+        <ul>
+          <li>
+            Collaborated closely with a team of six engineers to design and
+            implement innovative solutions for algorithmic learning with a
+            front-end development web application.
+          </li>
+          <li>
+            Participated in Front-end Design and state management using ReactJS,
+            while troubleshooting foundational application bugs and handling
+            ticket management in a GitLab workflow.
+          </li>
+          <li>
+            Designed and diagnosed NodeJS and Express HTTP server and queries
+            for data delivery between a PostgreSQL database and ReactJS
+            front-end for user comments.
+          </li>
+        </ul>
+      </div>
+    ),
   },
   {
     id: 2,
